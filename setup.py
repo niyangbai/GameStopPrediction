a185897843faeafd5cc193547af24ad221e860e5
niyang.bai <81044007+niyangbai@users.noreply.github.com>
2022-04-15 18:04:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='haha',
+    description='code for my master thesis',
     author='Niyang Bai',
     license='MIT',
 )
