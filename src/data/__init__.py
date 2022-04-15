3b4fe9d8b3e168085f70bb8af49e359aca06f22d
niyang.bai <81044007+niyangbai@users.noreply.github.com>
2022-04-15 20:16:38 +0200

    Update __init__.py

---
+++ src/data/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ['foofactories', 'tallFoos', 'shortfoos', 'medumfoos',
+           'santaslittlehelperfoo', 'superawsomefoo', 'anotherfoo']
+           
+from foo.foofactories import fooFactory
+from foo.tallfoos import tallFoo
+from foo.shortfoos import shortFoo
