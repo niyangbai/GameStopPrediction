56b6dc7dd20c03e1c481538152eca1525c8daa1f
italian_goat <81044007+niyangbai@users.noreply.github.com>
2022-04-18 22:40:04 +0200

    update

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ['data', 'features', 'models', 'visualization']
 from src.data.make_dataset import RdtData
 from src.data.make_dataset import FinData
-from src.features.build_features import data_clean
+from src.features.build_features import DataClean
 from src.features.build_features import time_reformat
 
