e0a65468aa61a7c7931fdcfbf66b70b9b6d72d50
italian_goat <81044007+niyangbai@users.noreply.github.com>
2022-04-18 21:35:46 +0200

    update

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import re
 import emoji
 from datetime import datetime
 
@@ -24,15 +23,7 @@ def words_clean(dataframe):
 def emj_clean(string, delete: bool):
 
     if delete:
-        emoji_pattern = re.compile('['
-                                   u'\U0001F600-\U0001F64F'  # emoticons
-                                   u'\U0001F300-\U0001F5FF'  # symbols & pictographs
-                                   u'\U0001F680-\U0001F6FF'  # transport & map symbols
-                                   u'\U0001F1E0-\U0001F1FF'  # flags (iOS)
-                                   u'\U00002702-\U000027B0'
-                                   u'\U000024C2-\U0001F251'
-                                   ']+', flags=re.UNICODE)
-        return emoji_pattern.sub(r'', string)
+        emoji.replace_emoji(string, replace='')
     else:
         pass
 
