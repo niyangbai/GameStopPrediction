0f3b194d2dcaaa8ca28e9aba2dbfe0bba0ba32f6
italian_goat <81044007+niyangbai@users.noreply.github.com>
2022-04-18 08:26:40 +0200

    update

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -2,5 +2,5 @@ __all__ = ['get_data',
            'data_clean',
            'nlp',
            'make_dataset']
-from get_data import Api_getdata
+from get_data import ApiGetData
 from data_clean import data_clean
