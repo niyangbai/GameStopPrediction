ef3a92ce15cb58f2ee46e54b7e0e5954d52869e3
niyang.bai <81044007+niyangbai@users.noreply.github.com>
2022-07-14 10:47:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='code for my master thesis',
+    description='code for nlp',
     author='Niyang Bai',
     license='MIT',
 )
